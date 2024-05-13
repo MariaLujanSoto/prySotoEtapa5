bcaeb54aefb9f377d126ede7554a8ee05a340ddb
marialujansoto <lujisotoflores@outlook.es>
2024-05-13 00:46:05 -0300

    Agregar archivos de proyecto.

---
+++ frmEtapa5.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace prySotoEtapa5
+{
+    partial class frmEtapa5
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(176, 569);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(263, 44);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // frmEtapa5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::prySotoEtapa5.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(619, 649);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "frmEtapa5";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmEtapa5_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
+
---
+++ frmEtapa5.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySotoEtapa5
+{
+    public partial class frmEtapa5 : Form
+    {
+        public frmEtapa5()
+        {
+            InitializeComponent();
+        }
+
+        bool Auto = false;
+        bool Avion = false;
+        bool Barco = false;
+        clsVehiculo objAuto = new clsVehiculo(); 
+        clsVehiculo objAvion = new clsVehiculo();
+        clsVehiculo objBarco = new clsVehiculo();
+
+        Random rnd = new Random();
+ 
+
+        private void frmEtapa5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MostrarImagenAleatoria()
+        {
+     
+        }
+
+        private PictureBox imagenAleatoria;
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            int indiceAleatorio = rnd.Next(1,4);
+            // eliminar el pct si ya existe uno
+            if (Controls.Contains(imagenAleatoria))
+            {
+                Controls.Remove(imagenAleatoria);
+                imagenAleatoria.Dispose(); 
+            }
+
+            switch (indiceAleatorio)
+            {
+                case 1:
+                    objAuto.crearAuto();
+                    objAuto.Auto.Location = new Point(400, 500);
+                    imagenAleatoria = objAuto.Auto;
+
+                    break;
+
+                case 2:
+                    objAvion.crearAvion();
+                    objAvion.Avion.Location = new Point(200, 100);
+                    imagenAleatoria = objAvion.Avion;
+
+                    break;
+
+                case 3:
+                    objBarco.crearBarco();
+                    objBarco.Barco.Location = new Point(300, 350);
+                    imagenAleatoria = objBarco.Barco;
+
+                    break;
+            }
+            Controls.Add(imagenAleatoria);
+
+
+        }
+    }
+}
